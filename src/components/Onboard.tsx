db0c1e5286a00467892b7c54afba2e9d7c109f06
jiyoon <vywns9978@gmail.com>
2023-11-12 17:55:21 +0900

    style: 온보딩 페이지 스타일링 수정

--- src/components/Onboard.tsx
+++ src/components/Onboard.tsx
@@ -6,11 +6,11 @@ export default function Onboard() {
 
   return (
     <section className="bg-primary  text-center flex justify-center items-center h-auto selection:bg-red_color">
-      <div className="relative mt-20 w-[1057px] h-[611px]">
-        <span className=" text-[20rem]  font-outline-red whitespace-pre-linere leading-[15rem] font-yeseva text-primary">
+      <div className="relative mt-20 w-[1057px] h-[600px]">
+        <span className=" text-[19rem]  font-outline-red whitespace-pre-linere leading-[15rem] font-yeseva text-primary ">
           {text}
         </span>
-        <div className="font-shrik text-red_color text-6xl leading-[2.8rem] absolute top-1/2 right-1/2 translate-x-1/2 translate-y-[-100%] ">
+        <div className="font-shrik text-red_color text-6xl leading-[2.8rem] absolute top-1/2 right-1/2 translate-x-1/2 translate-y-[-120%] ">
           Front-end
           <br /> Developer
         </div>
