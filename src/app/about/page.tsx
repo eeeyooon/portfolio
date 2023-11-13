30cdf95df1c784afe8c5b4317a094a6621d52dff
jiyoon <vywns9978@gmail.com>
2023-11-13 16:07:36 +0900

    style: about 페이지 스타일링 적용

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,37 @@
+import Image from "next/image";
+import ProfileImage from "../../../public/images/profile.png";
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <>
+      <section className="mt-20 mx-20 mb-12">
+        <h2 className="font-hambak text-red_color text-7xl mb-8">About Me</h2>
+        <div className="flex flex-row-reverse h-full justify-center">
+          <Image
+            src={ProfileImage}
+            alt="Pictiure of the author"
+            width={400}
+            height={300}
+            priority
+          />
+          <div className="pt-20 mr-20 ">
+            <p className="font-scoreRegular text-4xl text-gray-600 mb-4">
+              별 하나에 추억과 별 하나에 어머니
+            </p>
+            <p className="text-3xl">
+              별 하나에 추억과 별 하나에머니, 어머님, 나는 별 하나에 아름다운 말
+              한 마디씩 불러 봅니다. 그러나, 겨울이 지나고 나의 별에도 봄이
+              오면, 무덤 위에 파란 잔디가 피어나듯이 내 이름자 묻힌 언덕 위에도
+              자랑처럼 풀이 무성할 거외다. 별 하나에 추억과 별 하나에 사랑과 별
+              하나에 쓸쓸함과 별 하나에 동경과 별 하나에 시와 별 하나에 어머니,
+              어머니, 어머님, 나는 별 하나에 아름다운 말 한 마디씩 불러 봅니다.
+              어머니, 어머님, 나는 별 하나에 아름다운 말 한 마디씩 불러 봅니다.
+            </p>
+            <p className="text-3xl mt-14">
+              나는 별 하나에 아름다운 말 한 마디씩 불러 봅니다.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Onboard from "@/components/Onboard";
+import AboutPage from "./about/page";
 
 export default function Home() {
   return (
     <>
       <Onboard />
+      <AboutPage />
     </>
   );
 }
