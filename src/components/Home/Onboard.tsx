73f7a1c09affc75286a5bf80f36a85da1cecbb2c
jiyoon <vywns9978@gmail.com>
2023-11-27 19:46:23 +0900

    style: 온보딩 스타일 수정

--- src/components/Home/Onboard.tsx
+++ src/components/Home/Onboard.tsx
@@ -3,14 +3,15 @@ import useTypeword from "@/hooks/useTypeword";
 import Link from "next/link";
 
 export default function Onboard() {
-  const text = useTypeword("eeeyooon", 100);
+  const text = useTypeword("eeeyooon.", 60);
 
   return (
     <section className="bg-primary h-screen text-center flex flex-col justify-center items-center selection:bg-red_color">
-      <p className="font-shrik text-red_color md:text-7xl text-5xl">{text}</p>
+      <p className="font-shrik text-brown_color md:text-7xl text-5xl">{text}</p>
       <div className="text-xl md:text-2xl h-10 mt-4">
-        <p className="font-bold">
-          프론트엔드 개발자 <br />강지윤입니다.
+        <p>
+          프론트엔드 개발자 <br />
+          <span className="font-bold">강지윤</span>입니다.
         </p>
       </div>
       <div className="flex flex-row gap-5 text-xs font-bold md:text-base mt-8">
