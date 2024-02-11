adda5f608a1b27710344f0fdb2f867c683fbefd2
jiyoon <vywns9978@gmail.com>
2024-02-11 14:30:50 +0900

    style: header 반응형 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -32,7 +32,7 @@ export default function Header() {
       <nav
         className={`absolute top-[5vh] transition-all duration-300 ease-in-out md:static md:opacity-100 left-0 w-full md:w-fit  ${
           toggleHeader
-            ? "top-[12px] opacity-100 md:top-0 pointer-events-auto mt-2"
+            ? "top-[3vh] opacity-100 md:top-0 pointer-events-auto"
             : "top-[-200vh] opacity-0  pointer-events-none lg:pointer-events-auto md:pointer-events-auto"
         } font-dohyeon text-gray-600 `}
       >
