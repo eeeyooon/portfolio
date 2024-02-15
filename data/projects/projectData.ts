aecf5ad7334faba18c161e92bc3452d9ff30f443
jiyoon <vywns9978@gmail.com>
2024-02-15 16:02:22 +0900

    chore: 프로젝트 데이터 추가 및 이미지 추가

--- data/projects/projectData.ts
+++ data/projects/projectData.ts
@@ -75,7 +75,7 @@ export const projects: Projects[] = [
     category: ["team", "front"],
     path: "jeju-brokerage",
     featured: true,
-    stacks: ["react", "styledComponents", "axios", "spring"],
+    stacks: ["react", "styledComponents", "axios", "spring", "kakao"],
     githubUrl: "https://github.com/goormthon/frontend",
     docsUrl:
       "https://www.notion.so/eeeyooon/7436eeea62724ae1bbc26bce91980b50?pvs=4",
@@ -202,5 +202,28 @@ export const projects: Projects[] = [
       "https://eeeyooon.notion.site/b4286d82d0e74826a974d4873e7f72b3?pvs=74",
     serviceUrl: "https://eeeyooon-emotion-diary.web.app/",
     images: 3,
+  },
+  {
+    title: "포트폴리오 사이트",
+    intro: "프론트엔드 포트폴리오 사이트",
+    description:
+      "프론트엔드 개발자를 준비하면서 진행했던 프로젝트, 프론트엔드 개발 스택 등을 한번에 확인할 수 있는 포트폴리오 사이트를 구현하였습니다. 편리한 탐색을 위해 헤더 메뉴 혹은 화살표 버튼을 통한 스크롤 이동이 가능하도록 구현하며 각각의 프로젝트를 클릭하면 세부 내용을 확인할 수 있는 페이지로 이동합니다.",
+    points: [
+      "타이핑 효과를 주는 커스텀 훅을 만들어 로고에 타이핑 효과를 적용하고, `FramerMotion`을 사용하여 슬롯 머신 효과를 구현.",
+      "프로젝트 이미지 슬라이더 기능 구현.",
+      "이메일, 제목, 본문만 입력하면 간편하게 이메일을 보낼 수 있는 기능 구현.",
+      "다양한 기기나 화면의 크기에 따라 자동으로 맞춤 렌더링이 되도록 반응형 웹 디자인을 구현함.",
+      "Header의 메뉴를 클릭하거나 화살표 버튼을 통한 스크롤 이동을 구현하여 편리한 탐색이 가능함.",
+    ],
+    startDate: "2023-11-09",
+    endDate: "2024-02-10",
+    category: ["personal", "front"],
+    path: "portfolio-site",
+    featured: true,
+    stacks: ["nextjs", "typescript", "tailwindcss", "vercel"],
+    githubUrl: "https://github.com/eeeyooon/portfolio",
+    docsUrl: "",
+    serviceUrl: "https://portfolio-eeeyooon.vercel.app/",
+    images: 1,
   },
 ];
--- src/components/projects/ProjectGrid.tsx
+++ src/components/projects/ProjectGrid.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function ProjectGrid({ projects }: Props) {
   const sortedProjects = projects.sort((a, b) =>
-    a.startDate > b.startDate ? -1 : 1
+    a.endDate > b.endDate ? -1 : 1
   );
   return (
     <ul className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8 mb-14 font-scoreRegular">
