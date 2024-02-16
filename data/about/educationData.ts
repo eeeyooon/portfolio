b1c414dc46948890478c954cb6268a449fa980af
jiyoon <vywns9978@gmail.com>
2024-02-16 17:21:45 +0900

    feat: experience, education 컴포넌트 생성 및 데이터 추가

---
+++ data/about/educationData.ts
@@ -0,0 +1,42 @@
+import { experience } from "@/types/experience";
+
+export const educationData: experience[] = [
+  {
+    experienceName: "쌍용IT교육센터",
+    description: "AWS 클라우드 활용 Java 풀스택 개발자 양성 과정",
+    startDate: "2022-03-03",
+    endDate: "2022-08-12",
+    stacks: [
+      "html",
+      "css",
+      "javascript",
+      "jquery",
+      "java",
+      "oracle",
+      "springframework",
+      "spring",
+    ],
+    points: [
+      "JSP / Servlet으로 MVC 패턴 개발 경험이 있습니다.",
+      "Oracle Cloud를 활용하여 팀 DB 관리 및 AJAX로 데이터 작업을 해본 적이 있습니다.",
+      "Oracle SQL로 DML, DDL, DCL 등 다양한 문법 사용 경험이 있습니다.",
+      "myBatis를 활용한 Spring Framework 팀 프로젝트 경험이 있습니다.",
+    ],
+    urls: [""],
+  },
+  {
+    experienceName: "멋쟁이 사자처럼",
+    description: "프론트엔드 스쿨 4기",
+    startDate: "2022-11-28",
+    endDate: "2023-03-29",
+    stacks: ["html", "css", "javascript", "react"],
+    points: [
+      "심화 CSS 및 웹 표준과 웹 접근성을 지키는 마크업을 학습하였습니다.",
+      "반응형 웹 디자인(RWD)을 구현하였습니다.",
+      "코어 JavaScript와 함수형 프로그래밍에 대해 학습하였습니다.",
+      "React Hooks와 상태 관리 라이브러리, CSS in JS 라이브러리를 학습하고 사용하였습니다.",
+      "Github의 위키, 마일스톤, 백로그 등을 활용한 협업 경험이 있습니다. VanillaJS 팀 프로젝트와 React 팀 프로젝트 경험이 있습니다.",
+    ],
+    urls: [""],
+  },
+];
