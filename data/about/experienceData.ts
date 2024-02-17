ac543d906051d5797c385eea04b9d61ae58b8b12
jiyoon <vywns9978@gmail.com>
2024-02-17 15:47:26 +0900

    chore: experienceData 추가

---
+++ data/about/experienceData.ts
@@ -0,0 +1,45 @@
+import { experience } from "@/types/experience";
+
+export const experienceData: experience[] = [
+  {
+    experienceName: "구름톤 7기",
+    description: "카카오와 구름이 주관한 제 7회 구름톤(해커톤)",
+    startDate: "2023-09-05",
+    endDate: "2023-09-08",
+    stacks: ["react", "firebase", "kakao"],
+    points: [
+      "기획자, 디자이너, 개발자가 모여 3박 4일동안 아이디어를 도출하고 서비스를 만들어 내는 해커톤.",
+      "제주 농어업자와 외국인 근로자를 매칭하는 플랫폼 구현.",
+      "프론트엔드 개발자로 참여하여 최우수상 수상.",
+    ],
+    urls: [""],
+  },
+  {
+    experienceName: "코딩 마을 방범대",
+    description: "프론트엔드 개발자 소모임",
+    startDate: "2024-01-03",
+    endDate: "2024-02-21",
+    stacks: [""],
+    points: [
+      "[우아한 타입스크립트 with 리액트] 북 스터디 진행 완료.",
+      "[코어 자바스크립트] 북 스터디 진행중.",
+    ],
+    urls: ["https://github.com/Coding-Village-Protector"],
+  },
+  {
+    experienceName: "SKT와 함께하는 AI기반 인력 양성",
+    description:
+      "AI를 활용한 소프트웨어 서비스 상품 기획 및 개발을 위한 청년 인력 양성 프로그램",
+    startDate: "2021-08-23",
+    endDate: "2021-11-30",
+    stacks: ["python"],
+    points: [
+      "SKT의 Nugu AI 스피커를 활용한 서비스 상품 기획 및 개발.",
+      "기초 파이썬 학습 및 현직 개발자와의 멘토링.",
+      "팀이 함께 서비스 기획부터 실제 개발까지의 과정을 처음 경험하였고 개발자의 꿈을 가지게 되는 계기가 되었음.",
+    ],
+    urls: [
+      "https://eeeyooon.notion.site/Nugu-62e25bf5195344b88c228dddfc1a842d",
+    ],
+  },
+];
