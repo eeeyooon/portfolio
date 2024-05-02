121f968c23ea3f425d0523c4037380c34d14d4cc
jiyoon <vywns9978@gmail.com>
2024-05-02 16:14:35 +0900

    feat: framer motion 애니메이션 효과 적용

--- src/components/skills/SkillGrid.tsx
+++ src/components/skills/SkillGrid.tsx
@@ -1,16 +1,37 @@
+"use client";
+
 import { Skills } from "@/types/skills";
 import SkillCard from "./SkillCard";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 type Props = {
   skills: Skills[];
 };
 export default function SkillGrid({ skills }: Props) {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
+  const variants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
-    <ul className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 mt-8 mb-14">
-      {skills.map((skill) => (
-        <li key={skill.stack}>
+    <ul
+      ref={ref}
+      className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 mt-8 mb-14"
+    >
+      {skills.map((skill, index) => (
+        <motion.li
+          key={skill.stack}
+          variants={variants}
+          initial="hidden"
+          animate={isInView ? "visible" : "hidden"}
+          transition={{ delay: index * 0.2, duration: 0.8, ease: "easeInOut" }}
+        >
           <SkillCard skill={skill} />
-        </li>
+        </motion.li>
       ))}
     </ul>
   );
