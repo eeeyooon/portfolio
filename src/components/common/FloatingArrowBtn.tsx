b30fbb15334775af7cf61280b75a584c91939f36
jiyoon <vywns9978@gmail.com>
2024-05-07 18:56:25 +0900

    style: 플로팅 화살표 버튼에도 애니메이션 효과 적용

--- src/components/common/FloatingArrowBtn.tsx
+++ src/components/common/FloatingArrowBtn.tsx
@@ -1,7 +1,23 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { FaArrowAltCircleUp } from "react-icons/fa";
 
 export default function FloatingArrowBtn() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 1900);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isVisible) {
+    return null;
+  }
+
   return (
     <div className="w-7 h-7 md:w-9 md:h-9 mx-auto pulse-animation fixed z-20 bottom-10 right-10 md:right-14">
       <Link href="/">
