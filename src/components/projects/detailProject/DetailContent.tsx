aa71c396bbc2a2c2b15df53f3fec46bd38154ada
jiyoon <vywns9978@gmail.com>
2024-05-12 18:27:58 +0900

    chore: DetailContent 컴포넌트 분리

---
+++ src/components/projects/detailProject/DetailContent.tsx
@@ -0,0 +1,35 @@
+import MarkdownBox from "@/components/markdown/MarkdownBox";
+
+type ContentListItem = {
+  title: string;
+  detail?: string[];
+};
+
+type DetailContentProps = {
+  contentList: ContentListItem[];
+};
+
+export default function DetailContent({ contentList }: DetailContentProps) {
+  console.log(contentList);
+  return (
+    <ul className="text-base md:text-lg">
+      {contentList.map((item, idx) => (
+        <li key={`${item.title}-${idx}`}>
+          <dl>
+            <dt className="bg-stone-200 p-2 pl-4 mb-2 font-semibold">
+              {item.title}
+            </dt>
+            {item.detail?.map((detailItem, idx) => (
+              <dd
+                key={`${detailItem}-${idx}`}
+                className="list-item list-disc ml-6 marker:text-blue_color my-1 last:mb-4"
+              >
+                <MarkdownBox>{detailItem}</MarkdownBox>
+              </dd>
+            ))}
+          </dl>
+        </li>
+      ))}
+    </ul>
+  );
+}
