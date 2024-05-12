24663777eb37dc74e58aac6e92ea39a734f55e37
jiyoon <vywns9978@gmail.com>
2024-05-12 18:14:44 +0900

    style: 프로젝트 기여 부분 컴포넌트 생성 및 스타일링 적용

--- src/components/projects/detailProject/ProjectContribution.tsx
+++ src/components/projects/detailProject/ProjectContribution.tsx
@@ -1,4 +1,6 @@
+import MarkdownBox from "@/components/markdown/MarkdownBox";
 import { Project } from "@/types/project";
+import Image from "next/image";
 
 type ProjectContributionProps = {
   project: Project;
@@ -7,5 +9,38 @@ type ProjectContributionProps = {
 export default function ProjectContribution({
   project,
 }: ProjectContributionProps) {
-  return <section>ProjectContribution</section>;
+  return (
+    <section className="mt-12">
+      <h4 className="text-lg md:text-xl font-semibold md:font-bold mb-5 flex flex-row gap-2">
+        <Image
+          src="/images/icons/icon-sparkling.png"
+          alt="빛 아이콘"
+          width={25}
+          height={25}
+          style={{ width: 25, height: 25 }}
+          priority
+        />
+        <span>기여 부분</span>
+      </h4>
+      <ul className="text-base md:text-lg">
+        {project.contribution?.map((item, idx) => (
+          <li key={`${project} 기여도 ${idx}`}>
+            <dl>
+              <dt className="bg-stone-200 p-2 pl-4 mb-2 font-semibold">
+                {item.title}
+              </dt>
+              {item.detail?.map((detailItem, idx) => (
+                <dd
+                  key={`${detailItem}${idx}`}
+                  className="list-item list-disc ml-6 marker:text-blue_color my-1 last:mb-4"
+                >
+                  <MarkdownBox>{detailItem}</MarkdownBox>
+                </dd>
+              ))}
+            </dl>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
