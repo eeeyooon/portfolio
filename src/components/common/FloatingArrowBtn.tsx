362325580f4e4bcba03f3e3620a6412b92a4a3b1
eeeyooon <vywns9978@gmail.com>
2024-10-26 17:09:34 +0900

    style: floatingBtn 스타일 수정

--- src/components/common/FloatingArrowBtn.tsx
+++ src/components/common/FloatingArrowBtn.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { FaArrowAltCircleUp } from "react-icons/fa";
+import { IoIosArrowUp } from "react-icons/io";
 
 export default function FloatingArrowBtn() {
   const [isVisible, setIsVisible] = useState(false);
@@ -10,7 +10,7 @@ export default function FloatingArrowBtn() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsVisible(true);
-    }, 3600);
+    }, 6000);
     return () => clearTimeout(timer);
   }, []);
 
@@ -19,9 +19,9 @@ export default function FloatingArrowBtn() {
   }
 
   return (
-    <div className="w-7 h-7 md:w-9 md:h-9 mx-auto pulse-animation fixed z-20 bottom-10 right-10 md:right-14">
+    <div className="w-7 h-7 md:w-9 md:h-9 mx-auto animate-pulse fixed z-20 bottom-10 right-10 md:right-14">
       <Link href="/">
-        <FaArrowAltCircleUp className="text-gray-300 w-7 h-7  md:w-8 md:h-8  bg-white rounded-full" />
+        <IoIosArrowUp className=" w-6 h-6  md:w-7 md:h-7 " />
       </Link>
     </div>
   );
